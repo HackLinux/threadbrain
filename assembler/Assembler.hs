f2e46461e5ad56b9fa000ce279d4eabedd90f849
Kathy Spradlin <kathyspradlin@gmail.com>
2014-12-01 16:26:26 -0600

    Assembler assemble syncs better

--- assembler/Assembler.hs
+++ assembler/Assembler.hs
@@ -34,13 +34,14 @@ formatMifWidth width codes =
             | width == 32 = "\t" ++ (show $ length codes)  ++ ": " ++ "ffffffff;"
 
 assemble :: BFCode -> Either Error [MachineCode]
-assemble asm = filter ((/= "").mc) <$> concat <$> zipWithM (assembleLine linepcs) [0..] bfs
+assemble asm = filter ((/= "").mc) <$> concat <$> zipWithM (assembleLine linepcs syncNums) [0..] bfs
     where bfs = filter ((/="").bf) $ map BFCode $ lines $ bf asm
           linepcs = getLinePCs bfs
+          syncNums = getSyncNums bfs
 
-assembleLine :: [Int] -> Int -> BFCode -> Either Error [MachineCode]
-assembleLine startpcs line code = 
-    (reverse <$> ((MachineCode "a000":) <$> assembleLine' startpcs ((),[]) ((startpcs !! line) + (length (bf code)) -1) line ((BFCode . reverse . bf) code)))
+assembleLine :: [Int] -> [Int] -> Int -> BFCode -> Either Error [MachineCode]
+assembleLine startpcs syncNums line code = 
+    (reverse <$> ((MachineCode "a000":) <$> assembleLine' startpcs syncNums ((),[]) ((startpcs !! line) + (length (bf code)) -1) line ((BFCode . reverse . bf) code)))
     >>= (getEnds ((),[]) (startpcs !! line))
 
 
@@ -55,8 +56,8 @@ getEnds ((), stack) pc (x:xs)
 getEnds _ _ [] = Right []
 
 
-assembleLine' :: [Int] -> ((), Stack) -> Int -> Int -> BFCode -> Either Error [MachineCode]
-assembleLine' startpcs ((), stack) pc line (BFCode (x:xs)) = case x of
+assembleLine' :: [Int] -> [Int] -> ((), Stack) -> Int -> Int -> BFCode -> Either Error [MachineCode]
+assembleLine' startpcs syncNums ((), stack) pc line (BFCode (x:xs)) = case x of
     '+' -> (MachineCode "1000":) <$> rest
     '-' -> (MachineCode "2000":) <$> rest
     '>' -> (MachineCode "3000":) <$> rest
@@ -64,7 +65,7 @@ assembleLine' startpcs ((), stack) pc line (BFCode (x:xs)) = case x of
     '[' -> case hex endJump of 
             Right end -> (MachineCode ("5" ++ end):) <$> restnext
             Left err -> Left err
-    ']' -> (MachineCode "6000":) <$> (assembleLine' startpcs (pushJump pc stack) (pc-1) line $ BFCode xs)
+    ']' -> (MachineCode "6000":) <$> (assembleLine' startpcs syncNums(pushJump pc stack) (pc-1) line $ BFCode xs)
     '*' -> case hex line of
             Right start -> (MachineCode ("7" ++ start):) <$> rest
             Left err -> Left err
@@ -75,18 +76,23 @@ assembleLine' startpcs ((), stack) pc line (BFCode (x:xs)) = case x of
                 else case hex (startpcs !! (line + 1)) of
                     Right start -> (MachineCode ("7" ++ start):) <$> rest
                     Left err -> Left err 
-    '|' -> (MachineCode "8000":) <$> rest
-    ' ' -> assembleLine' startpcs ((), stack) pc line $ BFCode xs
+    '|' -> case hexInt 8 (pc - (startpcs !! line)) of
+            Right col -> case hexInt 4 (syncNums !! (pc - (startpcs !! line))) of
+              Right num -> (MachineCode ("8" ++ num ++ col):) <$> rest
+              Left err -> Left err
+            Left err -> Left err
+    ' ' -> assembleLine' startpcs syncNums ((), stack) pc line $ BFCode xs
     '.' -> (MachineCode "9000":) <$> rest
-    where rest = assembleLine' startpcs ((), stack) (pc-1) line $ BFCode xs
+    where rest = assembleLine' startpcs syncNums ((), stack) (pc-1) line $ BFCode xs
           (endJump, nextstack) = getJump stack 
-          restnext = assembleLine' startpcs ((), nextstack) (pc-1) line $ BFCode xs
-assembleLine' _ _ _ _ (BFCode []) = Right []
+          restnext = assembleLine' startpcs syncNums ((), nextstack) (pc-1) line $ BFCode xs
+assembleLine' _ _ _ _ _ (BFCode []) = Right []
 
 toHex [] = []
 toHex bits = (hexDigit $ take 4 bits) : (toHex $ drop 4 bits)
 hexDigit = (intToDigit . readImmBase 2)
 hex str = toHex <$> getNLowestBits 12 False str
+hexInt int str = toHex <$> getNLowestBits int False str
 
 
 getNLowestBits :: Int -> Bool -> Int -> Either Error String
@@ -101,6 +107,20 @@ getNLowestBits n isSigned val
 
 getLinePCs :: [BFCode] -> [Int]
 getLinePCs bfs = scanl (\acc x -> acc + 1 + (length . bf) x) 0 bfs
+
+getSyncNums :: [BFCode] -> [Int]
+getSyncNums codes = map (getSyncNums' codes) [0..(maxCol-1)]
+    where maxCol = foldl (\acc x -> if (length . bf) x > acc then (length . bf) x else acc) 0 codes
+    
+
+getSyncNums' :: [BFCode] -> Int -> Int
+getSyncNums' codes col = foldl (getColSum col) 0 strings
+    where strings = map bf codes
+
+getColSum :: Int -> Int -> String -> Int
+getColSum col sum l 
+    | length l > col = if (l !! col) == '|' then sum + 1 else sum
+    | otherwise = sum
 
 pushJump :: Int -> Stack -> ((),Stack)
 pushJump a xs = ((), a:xs)
